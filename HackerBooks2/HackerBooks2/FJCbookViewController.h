ec1ee2e8df1ba9eb1d495eacdfc499fb868ba5cd
Javier Cazorla <sherlock722@gmail.com>
2016-01-26 01:41:24 +0100

    Creamos método de clase y controlador de la clase FJCBook

---
+++ HackerBooks2/HackerBooks2/FJCbookViewController.h
@@ -0,0 +1,13 @@
+//
+//  FJCbookViewController.h
+//  HackerBooks2
+//
+//  Created by Javier Cazorla Moriche on 26/1/16.
+//  Copyright © 2016 FJC. All rights reserved.
+//
+
+#import "AGTCoreDataTableViewController.h"
+
+@interface FJCbookViewController : AGTCoreDataTableViewController
+
+@end
---
+++ HackerBooks2/HackerBooks2/FJCbookViewController.m
@@ -0,0 +1,52 @@
+//
+//  FJCbookViewController.m
+//  HackerBooks2
+//
+//  Created by Javier Cazorla Moriche on 26/1/16.
+//  Copyright © 2016 FJC. All rights reserved.
+//
+
+#import "FJCbookViewController.h"
+#import "FJCBook.h"
+
+@implementation FJCbookViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    self.title = @"HackerBooks2";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    static NSString *cellID = @"NotebookCell";
+    
+    // Averiguar qué libreta es
+     FJCBook *book = [self.fetchedResultsController objectAtIndexPath:indexPath];
+    
+    // Crear la celda
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (cell == nil) {
+        // Creamos la celda de la nada
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellID];
+    }
+    // Sincronizar libreta -> celda
+    cell.textLabel.text = book.title;
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateStyle = NSDateFormatterShortStyle;
+    cell.detailTextLabel.text = [fmt stringFromDate:book.modificationBook];
+    
+    
+    // Devolvemos la celda
+    return cell;
+}
+
+
+
+@end
